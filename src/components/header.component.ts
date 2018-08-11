cef01305281796e9be79aa4d7265cbd0e7da0cd0
Serhii H <kingnlo@gmail.com>
2018-08-11 18:41:22 +0300

    Update header.component.ts

--- src/components/header.component.ts
+++ src/components/header.component.ts
@@ -3,8 +3,9 @@ import { DataTable } from './table.component';
 import { HEADER_TEMPLATE } from './header.template';
 import { HEADER_STYLE } from "./header.style";
 import {FormControl} from "@angular/forms";
-import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/distinctUntilChanged';
+import {debounceTime, distinctUntilChanged} from 'rxjs/operators';
+//import 'rxjs/add/operator/debounceTime';
+//import 'rxjs/add/operator/distinctUntilChanged';
 
 @Component({
   selector: 'data-table-header',
@@ -29,8 +30,8 @@ export class DataTableHeader implements OnInit {
 
     ngOnInit() {
         this.filterControl.valueChanges
-            .debounceTime(500)
-            .distinctUntilChanged()
+            .pipe(debounceTime(500))
+            .pipe(distinctUntilChanged())
             .subscribe((term: string) => {
                 this.filterChangeEvent.emit(term);
             });
